11b30095828803ce031187d7f361965ef20f8204
SrtaKennedy <ruthellen1800@gmail.com>
2022-10-09 21:03:59 -0300

    DesafioPOO da DIO

---
+++ Models/Xiaomi.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioPOO.Models
+{
+    public class Xiaomi : Smartphone
+    {
+        public Xiaomi (string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Xiaomi.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "564879", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "5644", modelo: "Modelo 2", imei: "22222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Xiaomi:");
+Smartphone Xiaomi = new Xiaomi(numero: "58974", modelo: "Modelo 3", imei: "33333333", memoria: 32);
+Xiaomi.ReceberLigacao();
+Xiaomi.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
